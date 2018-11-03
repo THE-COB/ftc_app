f67b2822a86173293b4830e6ff4293fc3e319f7e
AvesAblaze <avesablaze@gmail.com>
2018-11-03 13:12:46 -0400

    added one line

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorREVColorDistance.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorREVColorDistance.java
@@ -79,6 +79,7 @@ public class SensorREVColorDistance extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+        //comment
         // get a reference to the color sensor.
         sensorColor = hardwareMap.get(ColorSensor.class, "colorRange");
 
