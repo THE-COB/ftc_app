405a5742e2ee7f82ff1094289a6e0fb0719716d5
AvesAblaze <avesablaze@gmail.com>
2018-11-07 16:05:24 -0500

    Started BlueCrater class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCrater.java
@@ -15,6 +15,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+import java.util.concurrent.Callable;
+
 import static java.lang.Thread.sleep;
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
@@ -34,7 +36,7 @@ public class BlueCrater extends LinearOpMode {
 	float moveY;
 	float moveX;
 	float rotate;
-	int liftHeight;
+	int liftHeight = 0;
 	double sd;
 	String color;
 	Orientation angles;
@@ -43,6 +45,7 @@ public class BlueCrater extends LinearOpMode {
 		robot.init(hardwareMap);
 		waitForStart();
 		sleep(1);
+		//Deploys the robot down from when it is at the starting position
 		while(Math.abs(liftHeight-robot.getLiftHeight())<3450&&!gamepad1.a){
 			robot.lift("up");
 			telemetry.clearAll();
@@ -51,57 +54,47 @@ public class BlueCrater extends LinearOpMode {
 			telemetry.update();
 		}
 		robot.lift("stop");
-		sleep(100);
-		robot.moveLeftRight(-0.75);
+
+		//Once the robot reaches the floor it moves to the left and goes up. It then resets the linear slide
+		robot.moveLeftRight(0.75);
 		sleep(250);
 		robot.moveUpDown(1);
 		sleep(100);
 		robot.stopMotors();
-		sleep(100);
 		liftHeight=robot.getLiftHeight();
 		while(Math.abs(liftHeight-robot.getLiftHeight())<3300&&!gamepad1.a){
 			robot.lift("down");
 		}
 		robot.lift("stop");
+
+		//Moves up to a point where it is able to rotate and find the Vuforia
 		robot.moveUpDown(1);
 		sleep(200);
 		robot.stopMotors();
-		sleep(500);
 		robot.moveLeftRight(-0.3);
 		sleep(1500);
-		robot.rotate(0.1);
+		robot.rotate(-0.1);
 		sleep(200);
+		//If the robot is unable to find the Vuforia it continues to rotate
 		while(!robot.resetCoordinates()&&opModeIsActive()){
-			robot.rotate(-0.1);
+			robot.rotate(0.1);
 			sleep(200);
 			robot.stopMotors();
 			sleep(200);
 		}
 		robot.stopMotors();
 		telemetry.clearAll();
+
+		//Uses information from Vuforia and prints it and sets variables to use Vuforia information and calibrates the REV imu
 		robot.rotation=Orientation.getOrientation(robot.lastLocation, EXTRINSIC, XYZ, DEGREES);
 		telemetry.addData("angle", robot.rotation.thirdAngle);
 		telemetry.update();
-		sleep(500);
-		robot.rotate(-0.1);
 		robot.rotation=Orientation.getOrientation(robot.lastLocation, EXTRINSIC, XYZ, DEGREES);
-		int direction=-1;
-		robot.stopMotors();
-		sleep(1000);
-		robot.moveUpDown(0.15);
 		robot.translation=robot.lastLocation.getTranslation();
 		robot.rotateToAngle(135);
-		BNO055IMU.Parameters imuParameters;
-		imuParameters = new BNO055IMU.Parameters();
-		imuParameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-		imuParameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-		imuParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-		imuParameters.loggingEnabled      = true;
-		imuParameters.loggingTag          = "IMU";
-		imuParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-		robot.imu1.initialize(imuParameters);
+		robot.calibrate();
 		robot.startingAngle=135;
-		while(robot.resetCoordinates()&&opModeIsActive()&&Math.abs(Math.abs(robot.getY())+Math.abs(robot.getX()))<57){
+		while(robot.resetCoordinates()&&opModeIsActive()&&Math.abs(Math.abs(robot.getY())+Math.abs(robot.getX()))<59){
 			robot.moveUpDown(0.1);
 			robot.translation=robot.lastLocation.getTranslation();
 			telemetry.addData("x", robot.getX());
@@ -121,51 +114,60 @@ public class BlueCrater extends LinearOpMode {
 				robot.stopMotors();
 			}
 		}
-		robot.rotateToAngle(135);
 		robot.stopMotors();
-		sleep(1000);
-		//robot.moveLeftRight(0.8);
-		sleep(200);
-		while(!(robot.sensorDistance.getDistance(DistanceUnit.INCH)<20)&&opModeIsActive()&&robot.resetCoordinates()){
-			robot.moveLeftRight(0.2);
-		}
-		sleep(150);
+
+
+		sleep(10000);
+		Callable<Boolean> checkDistance=new Callable<Boolean>() {
+			public Boolean call() {
+				return !(robot.sensorDistance.getDistance(DistanceUnit.INCH)<20)&&opModeIsActive()&&robot.resetCoordinates();
+			}
+		};
+
+		//Reads color of the left material
+		robot.moveLeftRight(0.2, checkDistance);
+		sleep(100);
 		robot.stopMotors();
 		color=robot.checkColor();
-		sleep(1000);
 		robot.rotateToAngle(135);
-
-		//robot.moveLeftRight(0.8);
 		sleep(200);
+		//If the mineral is gold the robot will move forward towards the depot while pushing the gold mineral with it. It will then drop the marker.
+
 		if(color.equals("yellow")){
-			robot.drive(23,true,0.8);
+			/*
+			robot.drive(23,true,0.6);
 			sleep(700);
 			robot.stopMotors();
 			sleep(1500);
 			robot.rotateToAngle(180);
-			robot.moveLeftRight(-0.5);
-			sleep(200);
+			robot.moveLeftRight(0.5);
+			sleep(600);
 			robot.stopMotors();
 			robot.marker1.setPosition(0.3);
 			sleep(1000);
 			robot.marker1.setPosition(1);
 			robot.stopMotors();
+			*/
 		}
+
+		//If the mineral is not gold it will read color of the center mineral
 		else {
 			robot.moveLeftRight(0.2);
-			sleep(1000);
-			while (!(robot.sensorDistance.getDistance(DistanceUnit.INCH) < 20) && opModeIsActive() && robot.resetCoordinates()) {
-				robot.moveLeftRight(0.2);
-				telemetry.addData("distance", robot.sensorDistance.getDistance(DistanceUnit.INCH));
-				telemetry.update();
-			}
-			sleep(150);
+			sleep(700);
+			robot.moveLeftRight(0.2, checkDistance);
+			telemetry.addData("distance", robot.sensorDistance.getDistance(DistanceUnit.INCH));
+			telemetry.update();
+			sleep(100);
+			color=robot.checkColor();
 			robot.rotateToAngle(135);
+
+			//If the mineral is gold the robot will move forward towards the depot while pushing the gold mineral with it. It will then drop the marker.
 			robot.stopMotors();
 			sleep(1000);
 			sleep(200);
 			if(color.equals("yellow")){
-				robot.drive(29,true,0.8);
+				/*
+				robot.drive(29,true,0.6);
 				robot.stopMotors();
 				sleep(1500);
 				robot.rotateToAngle(180);
@@ -173,18 +175,19 @@ public class BlueCrater extends LinearOpMode {
 				robot.marker1.setPosition(0.3);
 				sleep(1000);
 				robot.marker1.setPosition(1);
+				*/
 			}
+			//If neither the left nor the center mineral is gold it will move to the right mineral
 			else {
 				robot.moveLeftRight(0.2);
 				sleep(1000);
-
-				while (!(robot.sensorDistance.getDistance(DistanceUnit.INCH) < 20) && opModeIsActive() && robot.resetCoordinates()) {
-					robot.moveLeftRight(0.2);
-					telemetry.addData("distance", robot.sensorDistance.getDistance(DistanceUnit.INCH));
-					telemetry.update();
-				}
+				robot.moveLeftRight(0.2, checkDistance);
+				telemetry.addData("distance", robot.sensorDistance.getDistance(DistanceUnit.INCH));
+				telemetry.update();
 				robot.stopMotors();
-				robot.drive(21, true,0.8);
+				//If the mineral is gold the robot will move forward towards the depot while pushing the gold mineral with it. It will then drop the marker.
+				/*
+				robot.drive(21, true,0.6);
 				robot.stopMotors();
 				sleep(1500);
 				robot.rotateToAngle(180);
@@ -192,15 +195,19 @@ public class BlueCrater extends LinearOpMode {
 				robot.marker1.setPosition(0.3);
 				sleep(1000);
 				robot.marker1.setPosition(1);
+				*/
 			}
 
 
 			robot.stopMotors();
-			color = "hi";
+
 
 		}
-		robot.rotateToAngle(265);
-		robot.drive(83, true,0.8);
+
+		//While the code is running it will constantly print the color, angle, lift height, and coordinates
+		// robot.rotateToAngle(265);
+		// robot.drive(63, true,0.8);
+		robot.stopMotors();
 		while (opModeIsActive()){
 			telemetry.addData("color", color);
 			telemetry.update();
@@ -224,22 +231,6 @@ public class BlueCrater extends LinearOpMode {
 			}
 
 
-		}
-	}
-	public void printStuff(){
-		if (robot.resetCoordinates()) {
-			telemetry.addData("Target", robot.currentTrackable.getName());
-			// express position (translation) of robot in inches.
-			robot.translation = robot.lastLocation.getTranslation();
-			//ArrayList translation[x, y, z]
-			telemetry.addData("x", robot.getX());
-			telemetry.addData("y", robot.getY());
-
-			// Map rotation firstAngle: Roll; secondAngle: Pitch; thirdAngle: Heading
-			robot.rotation = Orientation.getOrientation(robot.lastLocation, EXTRINSIC, XYZ, DEGREES);
-			telemetry.addData("Heading", robot.getAngle());
-		} else {
-			telemetry.addData("Target", "none");
 		}
 	}
 
