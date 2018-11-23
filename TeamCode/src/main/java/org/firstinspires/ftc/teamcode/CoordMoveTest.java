c8a1cc0f584913f971111b0d033ded77c5ca2759
AvesAblaze <avesablaze@gmail.com>
2018-11-23 12:31:05 -0500

    logistics

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoordMoveTest.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.concurrent.Callable;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+
+/**
+ * Created by Rohan Mathur on 9/26/18.
+ */
+@TeleOp(name="CoordMoveTest", group="Competition")
+
+public class CoordMoveTest extends AvesAblazeOpmode {
+
+	/* Declare OpMode members. */
+	// Use a Pushbot's hardware
+	private ElapsedTime runtime = new ElapsedTime();
+	public float moveY;
+	public float moveX;
+	public float rotate;
+	public int liftHeight = 0;
+	public double sd;
+	public String color;
+	public Orientation angles;
+
+	@Override
+	public void runOpMode() {
+		telemetry.addData("status", "calibrating");
+		telemetry.update();
+		robot.init(hardwareMap);
+		telemetry.clearAll();
+
+
+
+		while (opModeIsActive()){
+			telemetry.addData("color", color);
+			telemetry.update();
+			angles   = robot.imu1.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+			telemetry.addData("angle", angles.firstAngle);
+			telemetry.addData("height", getLiftHeight());
+			//Display coordinates and trackable
+			if (resetCoordinates()) {
+				telemetry.addData("Target", robot.currentTrackable.getName());
+				// express position (translation) of robot in inches.
+				robot.translation = robot.lastLocation.getTranslation();
+				//ArrayList translation[x, y, z]
+				telemetry.addData("x", getX());
+				telemetry.addData("y", getY());
+
+				// Map rotation firstAngle: Roll; secondAngle: Pitch; thirdAngle: Heading
+				robot.rotation = Orientation.getOrientation(robot.lastLocation, EXTRINSIC, XYZ, DEGREES);
+				telemetry.addData("Heading", robot.rotation.thirdAngle);
+			} else {
+				telemetry.addData("Target", "none");
+			}
+
+
+		}
+	}
+
+
+
+}
