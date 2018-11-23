2f7f85a7ef65303f512bf5bc9ceffa596c2ce14f
AvesAblaze <avesablaze@gmail.com>
2018-11-23 12:34:42 -0500

    Made program to test moveToCoord()

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoordMoveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoordMoveTest.java
@@ -40,7 +40,10 @@ public class CoordMoveTest extends AvesAblazeOpmode {
 		telemetry.update();
 		robot.init(hardwareMap);
 		telemetry.clearAll();
-
+		telemetry.addData("TheoreticalAngle", getAngle());
+		resetCoordinates();
+		moveToCoord(getX()+5,getY()+5,45,0.25);
+		stopMotors();
 
 
 		while (opModeIsActive()){
