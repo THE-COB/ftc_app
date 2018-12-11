d25d1d5efa45f1320a78b1a1ca1c1566a4bc2d5d
THE-COB <jmlife6@gmail.com>
2018-12-11 16:01:11 -0500

    small fixes on autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueDepotLightsNoDeploy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueDepotLightsNoDeploy.java
@@ -30,12 +30,17 @@ public class BlueDepotLightsNoDeploy extends AvesAblazeOpmode {
 			calibrate();
 
 			telemetry.addData("Gold Mineral", position);
-			telemetry.addData("position", position.equals("nothing"));
+			telemetry.addData("position", position.equals("none"));
 			telemetry.update();
 		}
-		robot.startingAngle=122;
-		if(position.equals("nothing")){
+
+		robot.startingAngle=120;
+		if(position.equals("none")){
 			position.equals("right");
+		}
+		while(opModeIsActive()&&!gamepad2.a&&!gamepad1.a){
+			telemetry.addData("position", position);
+			telemetry.update();
 		}
 		robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.fromNumber(18-1));
 
@@ -69,12 +74,10 @@ public class BlueDepotLightsNoDeploy extends AvesAblazeOpmode {
 		sleep(900);
 		robot.arm.setPower(0);
 		}
-		else{
-			moveUpDown(1);
-			sleep(100);
-			moveLeftRight(-1);
-			sleep(500);
-			rotateToAngle(155);
+		else if(position.equals("right")){
+			polarDrive(1, Math.PI/3-0.2);
+			sleep(1500);
+			rotateToAngle(165);
 			moveUpDown(1);
 			sleep(1200);
 			rotate(-1);
@@ -88,7 +91,7 @@ public class BlueDepotLightsNoDeploy extends AvesAblazeOpmode {
 			moveUpDown(-1);
 			sleep(100);
 			moveLeftRight(-1);
-			sleep(650);
+			sleep(450);
 			moveUpDown(-1);
 			sleep(2150);
 			stopMotors();
@@ -100,6 +103,36 @@ public class BlueDepotLightsNoDeploy extends AvesAblazeOpmode {
 			sleep(900);
 			robot.arm.setPower(0);
 		}
+		else{
+			polarDrive(1, 2*Math.PI/2.7);
+			sleep(820);
+			rotateToAngle(180);
+			moveUpDown(1);
+			sleep(420);
+			moveLeftRight(-1);
+			sleep(350);
+			robot.marker1.setPosition(0.3);
+			sleep(830);
+			robot.marker1.setPosition(1);
+			rotateToAngle(180);
+			moveLeftRight(1);
+			sleep(150);
+			moveUpDown(-1);
+			sleep(250);
+			moveLeftRight(-1);
+			sleep(250);
+			moveUpDown(-1);
+			sleep(2250);
+			stopMotors();
+			robot.extension.setPower(1);
+			sleep(1200);
+			robot.extension.setPower(0);
+			while(runtime.seconds()<28&&opModeIsActive());
+			robot.arm.setPower(1);
+			sleep(900);
+			robot.arm.setPower(0);
+		}
+
 		stopMotors();
 
 
